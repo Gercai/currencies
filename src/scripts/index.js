50c8713c7396b961f253d6d7a53a36508b1e5f28
geraldjän <solidcaius@gmail.com>
2020-03-02 13:36:32 +0100

    added fetch

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,13 @@
-// The following line makes sure your styles are included in the project. Don't remove this.
 import '../styles/main.scss';
-// Import any additional modules you want to include below \/
 
 
-// \/ All of your javascript should go here \/
+const baseURL = "https://api.cryptonator.com/api/ticker/btc-usd";
 
+fetch(baseURL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    let here = data;
+  });
